e25b521fbaa5efd7f69fdd88a3c575e364976d45
dominikmusewitsch <dmusewitsch@gmail.com>
2025-12-18 12:36:52 +0100

    initial commit

---
+++ asset-active/Grass Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Grass Tileset" tilewidth="64" tileheight="64" tilecount="256" columns="16">
+ <image source="../asset-packs/Foozle_2DT0012_Spire_Tileset_1/Tileset/PNGs/Grass Tileset.png" width="1024" height="1024"/>
+</tileset>
