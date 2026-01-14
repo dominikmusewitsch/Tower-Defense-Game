93c25421d2c749ea92923fed62212c686cc238cf
Tolpius <167006924+Tolpius@users.noreply.github.com>
2026-01-14 15:25:26 +0100

    refactor: refactored map init

--- game/src/game/scenes/Game.ts
+++ game/src/game/scenes/Game.ts
@@ -2,7 +2,7 @@ import { Scene } from "phaser";
 import { Enemy } from "../entities/enemy";
 import { Tower } from "../entities/tower";
 import handleTowerBuild from "../scripts/TowerBuild";
-
+import handleMap1Init from "../scripts/init/maps/map1";
 export class Game extends Scene {
 
 
@@ -72,63 +72,7 @@ export class Game extends Scene {
         });
 
         //Map Init
-        const map = this.make.tilemap({
-            key: "mapOne",
-        });
-        const tilesetGrass = map.addTilesetImage("GrassTileset", "grass");
-        const tilesetWater = map.addTilesetImage("AnimatedWaterTiles", "water");
-        const tilesetSolidGreen = map.addTilesetImage(
-            "solid_green",
-            "solidGreen"
-        );
-        if (tilesetGrass) {
-            const layerBackground = map.createLayer(
-                "Terrain_Background",
-                tilesetGrass,
-                0,
-                0
-            );
-            const layerPath = map.createLayer(
-                "Terrain_Path",
-                tilesetGrass,
-                0,
-                0
-            );
-            const layerCliffs = map.createLayer(
-                "Terrain_Cliffs",
-                tilesetGrass,
-                0,
-                0
-            );
-            const layerProps = map.createLayer("Props", tilesetGrass, 0, 0);
-            const layerDetails = map.createLayer("Details", tilesetGrass, 0, 0);
-        }
-        if (tilesetWater) {
-            const layerWater = map.createLayer(
-                "Terrain_Water",
-                tilesetWater,
-                0,
-                0
-            );
-        }
-        //Buildable Layer Init
-        this.layerBuildable = null;
-        if (tilesetSolidGreen) {
-            this.layerBuildable = map.createLayer(
-                "Buildable",
-                tilesetSolidGreen,
-                0,
-                0
-            );
-            this.layerHighground = map.createLayer(
-                "Highground",
-                tilesetSolidGreen,
-                0,
-                0
-            );
-            this.layerHighground?.setVisible(false);
-            // Disable visibility of buildable layer initially
-            this.layerBuildable && this.layerBuildable.setVisible(false);
+        handleMap1Init(this);
 
             // Setup click handler for buildable tiles12
             this.input.on(
@@ -154,7 +98,7 @@ export class Game extends Scene {
                         buildRangeIndicator.setVisible(false);
                 }
             );
-        }
+        
 
         this.events.on("tower-selected", (towerId: string, cost: number) => {
             console.log("Game scene received tower-selected:", towerId, cost);
@@ -183,9 +127,9 @@ export class Game extends Scene {
                 .setDepth(2);
         });
 
-        let buildRangeIndicator: Phaser.GameObjects.Graphics | null = null;
-
+        
         //Build Preview Event Listener
+        let buildRangeIndicator: Phaser.GameObjects.Graphics | null = null;
         this.input.on("pointermove", (pointer: Phaser.Input.Pointer) => {
             if (!this.buildMode || !this.layerBuildable || !this.buildPreview)
                 return;
@@ -235,16 +179,7 @@ export class Game extends Scene {
         });
 
         //Waypoints Init
-        const layerWaypoints = map.getObjectLayer("Waypoints");
-        this.waypoints = layerWaypoints.objects[0].polyline;
-        const startPoint = this.waypoints[1];
-
-        this.path = new Phaser.Curves.Path(startPoint.x, startPoint.y);
-
-        this.waypoints.forEach((point, index) => {
-            if (index === 0) return;
-            this.path.lineTo(point.x, point.y);
-        });
+      
         //Enemy Spawn Init
         this.time.addEvent({
             delay: 1000,
---
+++ game/src/game/scripts/init/maps/map1.ts
@@ -0,0 +1,61 @@
+import { Game } from "../../../scenes/Game";
+
+export default function handleMap1Init(scene: Game) {
+    // Map 1 specific initialization code can go here
+    const map = scene.make.tilemap({
+        key: "mapOne",
+    });
+    const tilesetGrass = map.addTilesetImage("GrassTileset", "grass");
+    const tilesetWater = map.addTilesetImage("AnimatedWaterTiles", "water");
+    const tilesetSolidGreen = map.addTilesetImage("solid_green", "solidGreen");
+    if (tilesetGrass) {
+        const layerBackground = map.createLayer(
+            "Terrain_Background",
+            tilesetGrass,
+            0,
+            0
+        );
+        const layerPath = map.createLayer("Terrain_Path", tilesetGrass, 0, 0);
+        const layerCliffs = map.createLayer(
+            "Terrain_Cliffs",
+            tilesetGrass,
+            0,
+            0
+        );
+        const layerProps = map.createLayer("Props", tilesetGrass, 0, 0);
+        const layerDetails = map.createLayer("Details", tilesetGrass, 0, 0);
+    }
+    if (tilesetWater) {
+        const layerWater = map.createLayer("Terrain_Water", tilesetWater, 0, 0);
+    }
+    //Buildable Layer Init
+    scene.layerBuildable = null;
+    if (tilesetSolidGreen) {
+        scene.layerBuildable = map.createLayer(
+            "Buildable",
+            tilesetSolidGreen,
+            0,
+            0
+        );
+        scene.layerHighground = map.createLayer(
+            "Highground",
+            tilesetSolidGreen,
+            0,
+            0
+        );
+        scene.layerHighground?.setVisible(false);
+        // Disable visibility of buildable layer initially
+        scene.layerBuildable && scene.layerBuildable.setVisible(false);
+
+        const layerWaypoints = map.getObjectLayer("Waypoints");
+        scene.waypoints = layerWaypoints.objects[0].polyline;
+        const startPoint = scene.waypoints[1];
+
+        scene.path = new Phaser.Curves.Path(startPoint.x, startPoint.y);
+
+        scene.waypoints.forEach((point, index) => {
+            if (index === 0) return;
+            scene.path.lineTo(point.x, point.y);
+        });
+    }
+}
